c5eb64243a3d73059970687b347e4b7d4ffe651b
Zeljko Kojic <kojicz983@gmail.com>
2018-12-26 11:39:14 +0100

    Date fixes

--- read2.py
+++ read2.py
@@ -6,7 +6,6 @@ import pandas as pd
 
 # start_time = time.process_time()
 
-
 listOfFiles = list()
 
 for root, dirs, files in os.walk('./2018'):
@@ -18,23 +17,8 @@ for file_path in listOfFiles:
     sheet = pd.read_excel(file_path, header=2)
     sheet = sheet.loc[:, ~sheet.columns.str.contains('^Unnamed', na=False)]
     headers = list(sheet.columns.values)[:7]
-    # sheet1 = pd.read_excel(file_path, header=0)
-    # sheet1 = sheet1.loc[:, ~sheet1.columns.str.contains('^Unnamed', na=False)]
-    # headers1 = list(sheet1.columns.values)[:5]
 
     week = int(file_path.split('-')[2])
-    
-    # if isinstance(headers1[2],int):
-    #     week = headers1[2]
-    # elif isinstance(headers1[3],int):
-    #     week = headers1[3]
-    # else:
-    #     print(type(headers1[2]),type(headers1[3]))
-    #     print(f'Error {headers1[2]} {headers1[3]} on {file_path}')
-    #     break
-
-    # end_day =  week * 7
-    # start_day = end_day - 7
 
     for date in headers:
         if isinstance(date, str):
@@ -58,41 +42,35 @@ for file_path in listOfFiles:
                         year = 2018
                 except:
                     print(f'Error {date} on {file_path}. {date_list} separator: {separator}')
-                    break
-                    
+                    continue
+
                 try:
-                    date = datetime.date(year, month, day)
+                    date_tmp = datetime.date(year, month, day)
                 except:
+
                     try:
-                        date = datetime.date(year, day, month)
+                        date_tmp = datetime.date(year, day, month)
                     except:
+                        print('Date format error')
                         print(type(date))
                         print(f'Error {date} on {file_path}')
-                        break       
-        weekNumber = date.isocalendar()[1]
-        if not weekNumber == week:
-            print(week,weekNumber)
+                        continue
+
+        elif isinstance(date, datetime.date):
+            date_tmp = date
+        else:
+            print(type(date))
+            raise Exception(f'Error {date} on {file_path}')
+
+        weekNumber = date_tmp.isocalendar()[1]
+        if weekNumber not in [week + 1, week, week - 1] :
+            print(week, weekNumber)
             print(f'Error {date} on {file_path}')
-            break       
-        # if isinstance(date, datetime.date):
-        #     # if not date or range(week):
-        #         print(f'Error {date} on {file_path}')
-        #         break       
-
-        #     d = datetime.date(2018, 1, 1) + datetime.timedelta(day_of_year)
-        #     if type(d) == type(date):
-        #         if d != date:
-        #             date = d
-        #     else:
-        #         if d != date.date():
-        #             date = d 
-        # else:
-        #     print(type(date))
-        #     print(f'Error {date} on {file_path}')      
-      
+            continue
 
 
-   
+
+
 
 
 
